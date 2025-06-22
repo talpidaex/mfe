90902767d7d53e20f67e5be6f9cbb306646fad70
talpidaex <bayrakogzhn@gmail.com>
2025-06-23 00:33:29 +0300

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Container - Host Title</h1>
+      <MarketingApp />
+    </div>
+  );
+};
+
+export default App;
