98826f29d8ea9a8ac66b9cfd48d3de91c64e0626
talpidaex <bayrakogzhn@gmail.com>
2025-06-23 23:27:23 +0300

    testing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 const App = () => {
+  console.log("*");
   return (
     <div>
       <h1>Container - Host Title</h1>
