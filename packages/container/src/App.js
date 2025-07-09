5ad58d71d0e98b2bc315105e373cf469be16369f
talpidaex <bayrakogzhn@gmail.com>
2025-07-09 14:59:54 +0300

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom/cjs/react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 const App = () => {
   return (
     <BrowserRouter>
       <div>
-        <h1>Container - Host Title!</h1>
         <Header />
         <MarketingApp />
       </div>
